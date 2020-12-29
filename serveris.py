1cb2b60c87b5869ba431fbebf16cb53385b810fd
IlonaDemchenko <ilona.demcenko@rvt.lv>
2020-12-29 10:50:11 +0000

    Zars 01

--- serveris.py
+++ serveris.py
@@ -1,13 +1,17 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import dati
 
 app = Flask(__name__)
 
+app.config ['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
     return "Labrīt!"
 
+@app.route('/api/v1/vielas')
+def vielas():
+  return jsonify(dati.vielas)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
